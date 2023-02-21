ec32858fa90fe601c18f38279979a2c0f0a0e480
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2023-02-21 11:39:04 -0500

    added routing

--- sorting-visualizer/src/App.js
+++ sorting-visualizer/src/App.js
@@ -1,12 +1,19 @@
 import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
 import SortingVisualizer from "./SortingVisualizer/SortingVisualizer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      {/* <HamburgerMenu /> */}
-      <SortingVisualizer />
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* <HamburgerMenu /> */}
+        <main>
+          <Routes>
+            <Route path="/" element={<SortingVisualizer />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
